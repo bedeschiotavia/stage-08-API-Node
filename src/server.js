1f2f8b7210e3201c27987bf64fe3ff6cfec74968
Otávia Bedeschi <bedeschi.otavia@gmail.com>
2024-03-25 18:40:12 +0000

    Request - Route params

--- src/server.js
+++ src/server.js
@@ -2,6 +2,15 @@ const express = require ("express")
 
 const app = express()
 
+app.get("/message/:id/:user", (request, response) => {
+  
+  const { id, user } = request.params
+  
+  response.send(`Message ID: ${id}.
+  User: ${user}
+  `)
+})
+
 const PORT = 3333;
 
 app.listen(PORT, () => console.log(`Server is runing on Port ${PORT}`))
