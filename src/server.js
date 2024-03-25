f7fd4b9c01173892e8f6e363e903d3e67183c352
Otávia Bedeschi <bedeschi.otavia@gmail.com>
2024-03-25 21:15:23 +0000

    POST method, routes and controllers

--- src/server.js
+++ src/server.js
@@ -1,21 +1,12 @@
 const express = require ("express")
 
+const routes = require("./routes")
+
 const app = express()
+app.use(express.json())
 
-app.get("/message/:id/:user", (request, response) => {
-  const { id, user } = request.params
-  
-  response.send(`Message ID: ${id}.
-  User: ${user}
-  `)
-})
+app.use(routes)
 
-app.get("/users", (request, response) =>{
-  const { page, limit } = request.query
-
-  response.send(`Página: ${page} | Limite: ${limit}`)
-})
-
-const PORT = 3333;
+const PORT = 3333
 
 app.listen(PORT, () => console.log(`Server is runing on Port ${PORT}`))
