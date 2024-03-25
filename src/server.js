fc2404f728d663b8770493276c7e49633001649e
Otávia Bedeschi <bedeschi.otavia@gmail.com>
2024-03-25 18:54:02 +0000

    Query Params

--- src/server.js
+++ src/server.js
@@ -3,12 +3,17 @@ const express = require ("express")
 const app = express()
 
 app.get("/message/:id/:user", (request, response) => {
-  
   const { id, user } = request.params
   
   response.send(`Message ID: ${id}.
   User: ${user}
   `)
+})
+
+app.get("/users", (request, response) =>{
+  const { page, limit } = request.query
+
+  response.send(`Página: ${page} | Limite: ${limit}`)
 })
 
 const PORT = 3333;
