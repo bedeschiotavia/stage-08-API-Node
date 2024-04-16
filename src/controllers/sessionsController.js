91537639684a81b73fa5442268dd6e268c6a0b93
Otávia Bedeschi <bedeschi.otavia@gmail.com>
2024-04-16 18:02:55 +0100

    SessionController created

---
+++ src/controllers/sessionsController.js
@@ -0,0 +1,26 @@
+const knex = require("../database/knex")
+const AppError = require("../utils/AppError")
+const { compare } = require("bcryptjs")
+
+class SessionsControler {
+  async create(request, response){
+    const { email, password } = request.body;
+
+    const user = await knex("users").where({ email }).first()
+
+    if(!user) {
+      throw new AppError("Invalid email or password", 401)
+    }
+
+    const passwordMatched = await compare(password, user.password)
+
+    if (!passwordMatched) {
+      throw new AppError("Invalid email or password", 401)
+    }
+
+    return response.json(user)
+  }
+
+}
+
+module.exports = SessionsControler;
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,13 @@ const { Router } = require ("express")
 const userRouter = require("./users.routes")
 const notesRouter = require("./notes.routes")
 const tagsRouter = require("./tags.routes")
+const sessionsRoutes = require("./sessions.routes")
 
 const routes = Router()
 
 routes.use("/users", userRouter)
+routes.use("/sessions", sessionsRoutes)
 routes.use("/notes", notesRouter)
 routes.use("/tags", tagsRouter)
-
 
 module.exports = routes;
